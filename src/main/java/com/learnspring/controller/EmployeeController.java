9348e058f37b0aaaafc260c09b411b4ad0c02abf
javabear3388 <jiler3388@gmail.com>
2022-09-19 20:32:30 -0400

    Employee controller and employee create html page

---
+++ src/main/java/com/learnspring/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.learnspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String createEmployee (Model model){
+
+        return "employee/employee-create";
+    }
+}
---
+++ src/main/java/com/learnspring/model/Employee.java
@@ -0,0 +1,26 @@
+package com.learnspring.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
