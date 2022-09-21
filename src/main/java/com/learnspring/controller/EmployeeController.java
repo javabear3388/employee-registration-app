2e76d608074f364a1332124f34dff99696e23cb2
javabear3388 <jiler3388@gmail.com>
2022-09-20 20:28:34 -0400

    Show states selections and capture new objects

--- src/main/java/com/learnspring/controller/EmployeeController.java
+++ src/main/java/com/learnspring/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.learnspring.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,4 +19,13 @@ public class EmployeeController {
         model.addAttribute("states", DataGenerator.getAllStates());
         return "employee/employee-create";
     }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee){
+        
+
+        return "/employee/employee-list";
+    }
+
+
 }
