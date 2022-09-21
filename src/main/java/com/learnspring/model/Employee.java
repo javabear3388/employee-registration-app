d2ee03ac47dccd22ed2fb131b3688c31619b3ef3
javabear3388 <jiler3388@gmail.com>
2022-09-20 22:39:21 -0400

    Validation part 1

--- src/main/java/com/learnspring/model/Employee.java
+++ src/main/java/com/learnspring/model/Employee.java
@@ -3,6 +3,10 @@ package com.learnspring.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,6 +16,12 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//    @NotNull //accepts everything except Null - but accepts ""
+//    @NotEmpty //doesn't accept Null but accepst " "
+//    @NotBlank// doesn't accept Null , empty String or only space
+
+    @NotBlank //(message = "") -> will display error in the counsel
+    @Size(max = 12, min =2) //tells controller to validate it
     private String firstName;
     private String lastName;
 
