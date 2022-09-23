4e591850d875c405cc4c15cd6a8949384115c6c6
javabear3388 <jiler3388@gmail.com>
2022-09-22 20:06:54 -0400

    Validations and use it in thymleaf

--- src/main/java/com/learnspring/controller/EmployeeController.java
+++ src/main/java/com/learnspring/controller/EmployeeController.java
@@ -4,11 +4,14 @@ import com.learnspring.boostrap.DataGenerator;
 import com.learnspring.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.model.IModel;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -22,7 +25,15 @@ public class EmployeeController {
     }
 
     @PostMapping("/list")
-    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee,
+                               BindingResult bindingResult, Model model){
+                                //binding result only after the object you want to validate
+
+        if (bindingResult.hasErrors()){ //without this validation statement, the app will crash
+             model.addAttribute("states", DataGenerator.getAllStates());
+            return "employee/employee-create"; //stay in the same page
+        }
+
         DataGenerator.saveEmployee(employee);
         model.addAttribute("employees", DataGenerator.readAllEmployees());
 
